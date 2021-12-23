e2397459779b632adf0b7c8b2c45e1bf7c53e531
Levi Lutz <levi6900@icloud.com>
2021-12-22 21:44:20 -0500

    Set tmp before each check

--- sidecar_wait_until_ready.sh
+++ sidecar_wait_until_ready.sh
@@ -5,13 +5,14 @@ set -e
 echo "Searching for service"
 for i in `seq 1 60`
 do
+    tmp=0
     lsof -i:8080 || tmp=$?
     if [ $tmp -eq 0 ]
     then
         echo "Service published on :8080"
         exit 0
     else
-        echo "No service found yet, $i seconds left"
+        echo "No service found yet, $i seconds elapsed"
     fi
     sleep 1
 done
