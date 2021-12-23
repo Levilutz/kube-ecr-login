cdd3c58f0a26651f16ad688455a7542a24e53eba
Levi Lutz <levi6900@icloud.com>
2021-12-22 21:10:28 -0500

    Add wait_until_ready script

---
+++ sidecar_wait_until_ready.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+echo "Searching for service"
+for i in `seq 1 60`
+do
+    lsof -i:8080 || tmp=$?
+    if [ $tmp -eq 0 ]
+    then
+        echo "Service published on :8080"
+        exit 0
+    else
+        echo "No service found yet, $i seconds left"
+    fi
+    sleep 1
+done
+
+echo "Failed to find service in 60 seconds"
+exit 1
